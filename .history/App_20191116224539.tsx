d35df6280c267c63ad19f6fd03bf587712a9da91
ph0ph0 <phillipalanlee@googlemail.com>
2019-11-17 08:59:12 +0000

    initial commit

---
+++ .history/App_20191116224539.tsx
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {Fragment, FunctionComponent} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type PageOneProps = {};
+const PageOne: FunctionComponent<PageOneProps> = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'orange',
+        borderColor: 'red',
+        height: 100,
+        width: 50,
+      }}>
+      <Text>CUNT</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  return <PageOne />;
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
